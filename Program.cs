58af83b5ef06cad77edbadbc82753a165ba6d8dc
Ha Hieu <116289298+HahieuPAC@users.noreply.github.com>
2023-02-08 17:11:14 +1400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -32,9 +32,8 @@ var web = new HtmlWeb()
     OverrideEncoding = Encoding.UTF8
 };
 
-// List gender for product
-// List giới tính cho 2 site Đồng Hồ Nữ và Đồng Hồ Nam
-var genders = new List<string>() { "nam", "nu" };
+//List mã loại sản phẩm
+var typeCodes = new List<int>() { 10001, 10002, 10003, 10004, 10005 };
 
 // List product crawl
 // List lưu danh sách các sản phẩm Crawl được
@@ -45,111 +44,22 @@ var spinner = new ConsoleSpinner();
 
 Console.WriteLine("Please do not turn off the app while crawling!");
 
-// Loop for 2 gender 
-// Lặp List gender
-foreach (var gender in genders)
+//Loop
+foreach (var typeCode in typeCodes)
 {
-    var requestUrl = baseUrl + $"/kieu-dang/{gender}";
+    var requestUrl = baseUrl + $"/display.do?cmd=getTCategoryMain&TCAT_CD={typeCode}";
 
     // Load HTML to document from requestUrl
     // Load trang web, nạp html vào document từ requestUrl
-    var documentForGetTotalPageMale = web.Load(requestUrl);
 
-    // Get total page from requestUrl
-    // Lấy tổng số trang từ requestUrl
-    var textTotalPage = documentForGetTotalPageMale
+    var documentForPagesTypeCode = web.Load(requestUrl);
+
+    // Get all Node Product
+    // Lấy tất cả các Node chứa thông tin của sản phẩm
+    var listNodeProductItem = documentForPagesTypeCode
         .DocumentNode
-        .QuerySelectorAll(".page-numbers > li")
-        .ToList()
-        .Where(s => !string.IsNullOrEmpty(s.InnerText))
-        .ToList()
-        .Last()
-        .InnerText
-        .RemoveBreakLineTab();
+        .QuerySelectorAll(".pro-wrap__obj")
+        .ToList();
 
-    int totalPage;
-    int.TryParse(textTotalPage, out totalPage);
-
-    // Loop each page
-    // Lặp qua từng trang
-    for (var i = 1; i <= totalPage; i++)
-    {
-        var requestPerPage = baseUrl + $"/kieu-dang/{gender}/page/{i}";
-
-        // Load HTML to document from requestPerPage
-        // Load trang web, nạp html vào document từ requestPerPage
-        var documentForListItem = web.Load(requestPerPage);
-
-        // Get all Node Product
-        // Lấy tất cả các Node chứa thông tin của sản phẩm
-        var listNodeProductItem = documentForListItem
-            .DocumentNode
-            .QuerySelectorAll("div.shop-container " +
-                              "> div.products " +
-                              "> div.product-small " +
-                              "> div.col-inner " +
-                              "> div.product-small " +
-                              "> div.box-text")
-            .ToList();
-
-        // Loop for each Node
-        // Lặp qua các Node
-        foreach (var node in listNodeProductItem)
-        {
-            // Get Link Detail of Product
-            // Lấy Link chi tiết của sản phẩm
-            var linkDetail = node
-                .QuerySelector("div.title-wrapper > p.name > a")
-                .Attributes["href"]
-                .Value
-                .RemoveBreakLineTab();
-
-            // Get Product Name
-            // Lấy tên của sản phẩm
-            var productName = node
-                .QuerySelector("div.title-wrapper")
-                .InnerText
-                .RemoveBreakLineTab();
-
-            // Get Product Price includes Orgin Price and Discount Price
-            // Lấy giá của sản phẩm bao gồm giá gốc và giá khuyển mãi
-            var priceText = node
-                .QuerySelector("div.price-wrapper > span.price")
-                .InnerText
-                .RemoveBreakLineTab()
-                .ReplaceMultiToEmpty(new List<string>() { "₫", ",", "&#8363;" });
-
-            var priceList = priceText?.Split(" ").ToList();
-
-            // Parse text to price and format
-            var orginPrice = priceList?.Count > 0 ? double.Parse(priceList[0]).ToString("N2") : 0.ToString("N2");
-            var discountPrice = priceList?.Count > 0 ? double.Parse(priceList[1]).ToString("N2") : 0.ToString("N2");
-
-            // Add Product to listDataExport
-            // Thêm sản phẩm vào listDataExport
-            listDataExport.Add(new ProductModel()
-            {
-                ProductName = productName,
-                Gender = gender.Equals("nam") ? "Nam" : "Nữ",
-                Currency = "VND",
-                DiscountPrice = discountPrice,
-                OrginPrice = orginPrice,
-                LinkDetail = linkDetail,
-                Author = "https://www.code-mega.com"
-            });
-
-            // Print Spinner
-            spinner.Turn(displayMsg: $"Crawling {listDataExport.Count} Item(s) | ", sequenceCode: 5);
-        }
-    }
+    Console.WriteLine(listNodeProductItem);
 }
-
-var fileName = DateTime.Now.Ticks + "_code-mega.xlsx";
-
-// Export data to Excel
-ExportToExcel<ProductModel>.GenerateExcel(listDataExport, savePathExcel + fileName, "Code Mega");
-
-Console.WriteLine($"Crawled {listDataExport.Count} Item(s)");
-Console.WriteLine($"Link saved file excel: " + savePathExcel + fileName);
-Console.WriteLine("DONE !!!");
-Console.WriteLine("Code Mega");
